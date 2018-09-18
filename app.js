c638417dcc12cbc3400396786056cbeb71b020f7
donny <donny.gabai@gmail.com>
2018-09-18 11:53:40 -0400

    updated

--- app.js
+++ app.js
@@ -1,7 +1,56 @@
 $(document).ready(function() {
 
+  // ------------ modal content -----------------
+
+  // Open the Modal
+  function openModal() {
+    $('#myModal').style.display = "block";
+  }
+
+  // Close the Modal
+  function closeModal() {
+    $('#myModal').style.display = "none";
+  }
+
+  // var slideIndex = 1;
+  // showSlides(slideIndex);
+  //
+  // // Next/previous controls
+  // function plusSlides(n) {
+  //   showSlides(slideIndex += n);
+  // }
+  //
+  // // Thumbnail image controls
+  // function currentSlide(n) {
+  //   showSlides(slideIndex = n);
+  // }
+  //
+  // function showSlides(n) {
+  //   var i;
+  //   var slides = $(".mySlides");
+  //   var dots = $(".demo");
+  //   if (n > slides.length) {
+  //     slideIndex = 1
+  //   }
+  //   if (n < 1) {
+  //     slideIndex = slides.length
+  //   }
+  //   for (i = 0; i < slides.length; i++) {
+  //     slides[i].style.display = "none";
+  //   }
+  //   for (i = 0; i < dots.length; i++) {
+  //     dots[i].className = dots[i].className.replace(" active", "");
+  //   }
+  //   slides[slideIndex - 1].style.display = "block";
+  //   dots[slideIndex - 1].className += " active";
+  // }
+  //
+
+
+  // ------------ mouse content -----------------
+
   $('.tool-cards div').mouseenter(function() {
-    $(this).css('background-color', '#ffffff');
+    $(this).css('background-color', 'transparent');
   });
 
   $('img[role="button"]').mouseenter(function() {
@@ -15,23 +64,29 @@ $(document).ready(function() {
   //-------mouse enter display widgets-----
 
   $('.first-tool').mouseenter(function() {
+    $(this).css('background-color', 'transparent');
     $('.instagram').css('visibility', 'visible');
+    $('#hammer').css('visibility', 'hidden');
   });
 
   $('.second-tool').mouseenter(function() {
     $('.email').css('visibility', 'visible');
+    $('#lathe').css('visibility', 'hidden');
   });
 
   $('.third-tool').mouseenter(function() {
     $('.gallery').css('visibility', 'visible');
+    $('#twisty').css('visibility', 'hidden');
   });
 
   $('.fourth-tool').mouseenter(function() {
     $('.about').css('visibility', 'visible');
+    $('#saw').css('visibility', 'hidden');
   });
 
   $('.fifth-tool').mouseenter(function() {
     $('.reviews').css('visibility', 'visible');
+    $('#chisel').css('visibility', 'hidden');
   });
 
   //---------end code block----------------
@@ -42,26 +97,31 @@ $(document).ready(function() {
   $('.first-tool').mouseleave(function() {
     $(this).css('background-color', '#523212');
     $('.instagram').css('visibility', 'hidden');
+    $('#hammer').css('visibility', 'visible');
   });
 
   $('.second-tool').mouseleave(function() {
     $(this).css('background-color', '#40230e');
     $('.email').css('visibility', 'hidden');
+    $('#lathe').css('visibility', 'visible');
   });
 
   $('.third-tool').mouseleave(function() {
     $(this).css('background-color', '#341e0e');
     $('.gallery').css('visibility', 'hidden');
+    $('#twisty').css('visibility', 'visible');
   });
 
   $('.fourth-tool').mouseleave(function() {
     $(this).css('background-color', '#27190d');
     $('.about').css('visibility', 'hidden');
+    $('#saw').css('visibility', 'visible');
   });
 
   $('.fifth-tool').mouseleave(function() {
     $(this).css('background-color', '#180c01');
     $('.reviews').css('visibility', 'hidden');
+    $('#chisel').css('visibility', 'visible');
   });
 
   //-----------on click events-----------
