8700562e844a15766643d15e34d6aedd3912911e
donny <donny.gabai@gmail.com>
2018-10-09 11:37:03 -0400

    review slides working

--- app.js
+++ app.js
@@ -12,39 +12,6 @@ $(document).ready(function() {
     $('#myModal').style.display = "none";
   }
 
-  // var slideIndex = 1;
-  // showSlides(slideIndex);
-  //
-  // // Next/previous controls
-  // function plusSlides(n) {
-  //   showSlides(slideIndex += n);
-  // }
-  //
-  // // Thumbnail image controls
-  // function currentSlide(n) {
-  //   showSlides(slideIndex = n);
-  // }
-  //
-  // function showSlides(n) {
-  //   var i;
-  //   var slides = $(".mySlides");
-  //   var dots = $(".demo");
-  //   if (n > slides.length) {
-  //     slideIndex = 1
-  //   }
-  //   if (n < 1) {
-  //     slideIndex = slides.length
-  //   }
-  //   for (i = 0; i < slides.length; i++) {
-  //     slides[i].style.display = "none";
-  //   }
-  //   for (i = 0; i < dots.length; i++) {
-  //     dots[i].className = dots[i].className.replace(" active", "");
-  //   }
-  //   slides[slideIndex - 1].style.display = "block";
-  //   dots[slideIndex - 1].className += " active";
-  // }
-  //
 
 
   // ------------ mouse content -----------------
@@ -200,14 +167,15 @@ $(document).ready(function() {
     $(modal).css('display', 'none');
   });
 
-  $(closeButton).click(function() {
-    $('.tool-cards div').css({
-      "background-color": "transparent"
-    });
-    $('.gallery, .about, .reviews').css({
-      "visibility": "visible"
-    });
+
+  $('body').each(function() {
+    if ($(window).width() < 600) {
+      $('.first-tool, .second-tool, .third-tool, .fourth-tool, .fifth-tool').off('mouseleave');
+      $('.first-tool, .second-tool, .third-tool, .fourth-tool, .fifth-tool').off('mouseenter');
+    }
   });
+
+
 
   // When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
